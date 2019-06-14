218123554a4a1bad4c8f560bb31eee77664a7a33
dreriggins <drevonne.riggins00@gmail.com>
2019-06-14 22:11:10 +0000

    test

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,12 @@
 class Quote < ApplicationRecord
+  validates :saying, presence: true, length: { maximum: 140, minimum: 3 }
+  validates :author, presence: true, length: { maximum: 50, minimum: 3 }
+end
+
+def create
+  @quote = Quote.create(quote_params)
+  if @quote.invalid?
+    flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+  end
+  redirect_to root_path
 end
